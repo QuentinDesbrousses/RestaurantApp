6facb89142a8c926fdb8e3aceece72cfe7363b1a
QuentinDesbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-01-03 21:17:41 +0100

    update allergene list

--- frontend/src/app/components/listes/allergene-list/allergene-list.component.ts
+++ frontend/src/app/components/listes/allergene-list/allergene-list.component.ts
@@ -1,12 +1,12 @@
-import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
-import {Ingredient} from "../../../models/ingredient";
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
-import {IngredientFormComponent} from "../../forms/ingredient-form/ingredient-form.component";
 import {ConfirmationFormComponent} from "../../forms/confirmation-form/confirmation-form.component";
 import {AllergeneFormComponent} from "../../forms/allergene-form/allergene-form.component";
 import {LiveAnnouncer} from "@angular/cdk/a11y";
-import {AllergenesComponent} from "../../../pages/allergenes/allergenes.component";
 import {Allergene} from "../../../models/allergene";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatSort, Sort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-allergene-list',
@@ -15,13 +15,48 @@ import {Allergene} from "../../../models/allergene";
 })
 export class AllergeneListComponent implements OnInit,AfterViewInit {
   @Input() allergenes : Allergene[] | undefined;
+  dataSource = new MatTableDataSource<Allergene>();
+  displayedColumns2 = ['ID','nom', 'categorie','modifier','supprimer'];
+
+  @ViewChild(MatPaginator) paginator : MatPaginator | undefined;
+  @ViewChild(MatSort) sort: MatSort | undefined;
 
   constructor(private _liveAnnouncer: LiveAnnouncer, private dialog : MatDialog) { }
 
   ngOnInit(): void {
+    if(this.allergenes){
+      this.dataSource = new MatTableDataSource<Allergene>(this.allergenes);
+    }
   }
 
   ngAfterViewInit() {
+    if (this.paginator){
+      this.dataSource.paginator = this.paginator;
+    }
+    if(this.sort){
+      this.dataSource.sort = this.sort;
+    }
+  }
+
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   //CRUD Allergene
