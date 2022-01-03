56bb1a75be4a66b4155e98b250c18491d02639cc
QuentinDesbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-01-03 23:06:11 +0100

    update utilisateurs form

--- frontend/src/app/components/listes/utilisateur-list/utilisateur-list.component.ts
+++ frontend/src/app/components/listes/utilisateur-list/utilisateur-list.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Allergene} from "../../../models/allergene";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort, Sort} from "@angular/material/sort";
+import {LiveAnnouncer} from "@angular/cdk/a11y";
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {AllergeneFormComponent} from "../../forms/allergene-form/allergene-form.component";
+import {ConfirmationFormComponent} from "../../forms/confirmation-form/confirmation-form.component";
+import {Utilisateur} from "../../../models/utilisateur";
 
 @Component({
   selector: 'app-utilisateur-list',
   templateUrl: './utilisateur-list.component.html',
   styleUrls: ['./utilisateur-list.component.css']
 })
-export class UtilisateurListComponent implements OnInit {
+export class UtilisateurListComponent implements OnInit, AfterViewInit {
+  @Input() utilisateurs : Utilisateur[] | undefined;
+  dataSource = new MatTableDataSource<Utilisateur>();
+  displayedColumns = ['ID','nom', 'prenom','description','email','supprimer'];
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator : MatPaginator | undefined;
+  @ViewChild(MatSort) sort: MatSort | undefined;
+
+  constructor(private _liveAnnouncer: LiveAnnouncer, private dialog : MatDialog) { }
 
   ngOnInit(): void {
+    if(this.utilisateurs){
+      this.dataSource = new MatTableDataSource<Utilisateur>(this.utilisateurs);
+    }
+  }
+
+  ngAfterViewInit() {
+    if (this.paginator){
+      this.dataSource.paginator = this.paginator;
+    }
+    if(this.sort){
+      this.dataSource.sort = this.sort;
+    }
+  }
+
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  supprimerUtilisateur(id:string){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "30%";
+    dialogConfig.data = {id: id,type:"utilisateur"};
+    this.dialog.open(ConfirmationFormComponent,dialogConfig);
+    console.log("Utilisateur n° "+id+" supprimé");
   }
 
 }
