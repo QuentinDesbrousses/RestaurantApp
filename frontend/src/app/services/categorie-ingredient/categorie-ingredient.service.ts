bbf9c0b3a62fc952f1a205b34141e25f44dc499e
CecileDestIG <cecile.destaing@etu.umontpellier.fr>
2022-01-08 01:38:33 +0100

    get ingredient by id

--- frontend/src/app/services/categorie-ingredient/categorie-ingredient.service.ts
+++ frontend/src/app/services/categorie-ingredient/categorie-ingredient.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Ingredient} from "../../models/ingredient";
 import {CategorieIngredient} from "../../models/categorie-ingredient";
+import { CatIngr } from 'src/app/models/cat_ingr';
 
 @Injectable({
   providedIn: 'root'
@@ -11,22 +12,22 @@ export class CategorieIngredientService {
   constructor(private http : HttpClient) { }
 
   getAllCategorieIngredient(){
-    return this.http.get<CategorieIngredient>("http://localhost:3000/categorie_ingredient/");
+    this.http.get<CatIngr>("http://localhost:3000/cat_ingredient/");
   }
 
   getCategorieIngredient(id : string){
-    return this.http.get<CategorieIngredient>("http://localhost:3000/categorie_ingredient/:"+id);
+    return this.http.get<CatIngr>("http://localhost:3000/cat_ingredient/"+id);
   }
 
   createCategorieIngredient(categorieIngredient : CategorieIngredient){
-    return this.http.post<CategorieIngredient>("http://localhost:3000/categorie_ingredient/",categorieIngredient);
+    return this.http.post<CategorieIngredient>("http://localhost:3000/cat_ingredient/",categorieIngredient);
   }
 
   modifyCategorieIngredient(id : string,categorieIngredient : CategorieIngredient){
-    return this.http.put<CategorieIngredient>("http://localhost:3000/categorie_ingredient/:"+id,categorieIngredient);
+    return this.http.put<CategorieIngredient>("http://localhost:3000/cat_ingredient/"+id,categorieIngredient);
   }
 
   deleteCategorieIngredient(id : string){
-    return this.http.delete<CategorieIngredient>("http://localhost:3000/categorie_ingredient/:"+id);
+    return this.http.delete<CategorieIngredient>("http://localhost:3000/cat_ingredient/"+id);
   }
 }
