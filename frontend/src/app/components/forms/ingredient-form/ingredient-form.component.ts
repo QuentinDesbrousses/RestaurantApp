2f0ba5f4c0ad8361b2c6b66951df1efa8566e8bd
QuentinDesbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-01-09 12:36:58 +0100

    fix pb

--- frontend/src/app/components/forms/ingredient-form/ingredient-form.component.ts
+++ frontend/src/app/components/forms/ingredient-form/ingredient-form.component.ts
@@ -29,7 +29,7 @@ export class IngredientFormComponent {
     this.IngredientForm  = new FormGroup({
       $id : new FormControl(null),
       nom : new FormControl('',Validators.required),
-      categorie : new FormControl('Légume',Validators.required),
+      categorie : new FormControl('',Validators.required),
       allergene : new FormControl(''),
       unite : new FormControl('',Validators.required),
       quantite : new FormControl('',Validators.required),
@@ -46,8 +46,9 @@ export class IngredientFormComponent {
     const nb : number = this.data.id;
     var id_al : number = 0;
     var id_cat_ingredient : number = 0;
-    this.allergenes.forEach(a=>{if(a.getNom()==this.IngredientForm.value.allergene){id_al = a.getId()}});
-    this.categories.forEach(a=>{if(a.getNom()==this.IngredientForm.value.categorie){id_cat_ingredient = a.getId()}});
+    this.allergenes.forEach(a=>{if(a.getNom()==this.IngredientForm.value.allergene){id_al = a.getId();console.log(a.getId(),id_al)}});
+    this.categories.forEach(a=>{if(a.getNom()==this.IngredientForm.value.categorie){id_cat_ingredient = a.getId();console.log(a.getId(),id_cat_ingredient)}});
+    console.log(id_al,id_cat_ingredient)
     let tmpIngredient = new Ingredient(
         this.IngredientForm.value.id,
         this.IngredientForm.value.nom,
