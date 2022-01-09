bc20dbe9a2109c18fa5d6337d57cbd86725becb1
QuentinDesbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-01-09 13:40:29 +0100

    fix form unselect pb

--- frontend/src/app/components/forms/allergene-form/allergene-form.component.ts
+++ frontend/src/app/components/forms/allergene-form/allergene-form.component.ts
@@ -16,30 +16,29 @@ export class AllergeneFormComponent implements OnInit {
   categories : CategorieAllergene[] = [];
 
   constructor(public service : AllergeneService,public sca : CategorieAllergeneService, public dialogRef: MatDialogRef<AllergeneFormComponent>,@Inject(MAT_DIALOG_DATA) public data: {id:number,type: string}) {
+    this.categories = this.sca.getAllCategorieAllergene();
     this.AllergeneForm = new FormGroup({
       $id : new FormControl(null),
       nom : new FormControl('',Validators.required),
-      categorie : new FormControl('')
+      categorie : new FormControl('',Validators.required)
     })
   }
 
   ngOnInit(){
-    this.categories = this.sca.getAllCategorieAllergene();
   }
 
   onSubmit(){
-    console.log("c'est id"+this.data.id);
     var cat : number = 0;
-    this.categories.forEach(c=>{if(this.AllergeneForm.value.categorie==c.getNom()){cat = c.getId()}})
+    this.categories.forEach(c=>{console.log(this.AllergeneForm.value.categorie,c.getNom());if(this.AllergeneForm.value.categorie==c.getNom()){cat = c.getId()}})
     const nb : number = this.data.id;
-    this.sca.deleteCategorieAllergene(nb);
-    let tmpAllergene = new Allergene(this.AllergeneForm.value.id,this.AllergeneForm.value.nom,cat);
+    console.log(cat)
+    let tmpAllergene = new Allergene(nb,this.AllergeneForm.value.nom,cat);
     if(this.data.type == "creation"){
       this.service.createAllergene(tmpAllergene);
       console.log("Allergène créé : "+tmpAllergene);
     }
     else if(this.data.type == "modification"){
-      this.service.modifyAllergene(this.AllergeneForm.value.id,tmpAllergene)
+      this.service.modifyAllergene(nb,tmpAllergene)
       console.log("Allergène modifié : "+tmpAllergene);
     }
     else{
