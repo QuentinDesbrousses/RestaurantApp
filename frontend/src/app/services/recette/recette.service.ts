8d26add7b5f9768fbb577bf11004a92fe68091f6
CecileDestIG <cecile.destaing@etu.umontpellier.fr>
2022-01-10 17:05:09 +0100

    services dans recette

--- frontend/src/app/services/recette/recette.service.ts
+++ frontend/src/app/services/recette/recette.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Recette} from "../../models/recette";
 import {ServicesConfigComponent} from "../services-config";
 import {CategorieIngredient} from "../../models/categorie-ingredient";
+import { Ingredient } from 'src/app/models/ingredient';
 
 @Injectable({
   providedIn: 'root'
@@ -37,6 +38,33 @@ export class RecetteService {
     );
     return tab;
   }
+
+  getIngredientByEtape(id : number){
+    var tab:any[]=[];
+    this.http.get<any>(ServicesConfigComponent.url+"etape/ingredientpretape/"+id).subscribe(
+      data=>{ 
+        data.forEach((element: any) => { tab.push({
+          ingredient: new Ingredient(element.id_ingredient, element.nom_ingredient, element.id_cat_ingr, 
+            element.unite, element.quantite_stock, element.cout_unitaire,
+            element.id_allergene),quantite:element.quantite
+        }) 
+      })}
+    );
+    return tab;
+  }
+
+  getRecettesByRecette(id:number){
+    var res : Recette[] = [];
+    let req = this.http.get<any[]>(ServicesConfigComponent.url+"recette/recetteByRecette/").subscribe(
+        data => {
+          data.forEach(e =>{
+            res.push(new Recette(e.id_recette,e.id_categorie,e.id_utilisateur,e.titre_recette,e.description_recette,e.nb_couvert,e.temps_recette));
+          })},
+        error => {console.log("error : "+error)});
+    console.log("res : "+res)
+    return res
+  }
+
 
   createRecette(recette : any){
     return this.http.post<any>(ServicesConfigComponent.url+"recette/",recette).subscribe(
